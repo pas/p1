9492a095cb0c2ecb917c365ab2dff071563616d6
Pas <pascal@offenes-lernen.ch>
2010-10-02 01:02:24 +0200

    first commmit

---
+++ uebung1/src/Output.java
@@ -0,0 +1,16 @@
+/*
+ *  
+ *  Author: 		Judith Fuog / Pascal Zaugg
+ *  Matrikelnr.:	?? / 05-299-425
+ *  Aufgabe:		1-3a
+ *  Program:		Output.java
+ *  
+ */
+
+public class Output {
+	public static void main(String[] args) {
+       System.out.println("1 + 2"); 			// gibt "1 + 2"
+       System.out.println(1 + 2);				// gibt 3
+       System.out.println("1 + 2 = " + 2 + 3);	//gibt "1 + 2 = 23"
+	}
+}
