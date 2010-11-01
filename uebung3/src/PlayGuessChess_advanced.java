fc1581921f345c0b91542457c52fad7deab1bc10
Pas <pascal@offenes-lernen.ch>
2010-11-01 23:05:12 +0100

    A advanced version of GuessChess.java. Input is checked and user forced to try again. Size of Table can be changed from 1 to 25. Maximum Guesses can be changed. Results are saved in a file and reloaded in next game (ATTENTION: only until tablesize is changed, then the results are overritten). Score in percentage as well.

---
+++ uebung3/src/PlayGuessChess_advanced.java
@@ -0,0 +1,8 @@
+public class PlayGuessChess_advanced {
+
+  public static void main (String[] args) {
+    GuessChess_advanced gChess = new GuessChess_advanced(8, 8, 3);
+    gChess.play();
+  }
+	
+}
