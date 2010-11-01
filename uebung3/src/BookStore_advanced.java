f1235d0da6ae47fd50bfb36a5ab9ac093cbbb0ce
Pas <pascal@offenes-lernen.ch>
2010-11-01 22:35:14 +0100

    A more sophisticated version of BookStore.java using arrays.

---
+++ uebung3/src/BookStore_advanced.java
@@ -0,0 +1,39 @@
+/*
+ * Using array to store books
+ */
+
+public class BookStore_advanced {
+	private Book[] Books = new Book[5];
+		
+	public BookStore_advanced() {
+	}
+	
+	public void registerBooks() throws java.text.ParseException {
+	  for (int i = 0 ; i<Books.length; i++) {
+		  Books[i] = new Book();
+		  Books[i].input();
+		  System.out.print("Do you want to register another book (y/n)? ");
+	      java.util.Scanner scn = new java.util.Scanner(System.in);
+		  if (scn.nextLine().equals("n")) {
+	        break;
+	      }
+	   }	  
+	}
+	
+	public void listBooks() {
+		for (int i = 0 ; i<Books.length; i++) {
+				  if (Books[i] != null) System.out.println(Books[i] + "");
+		 }
+	}
+	
+	public String getCheapestBook() {
+		Book cheapest = Books[0];
+		
+		for (int i = 1 ; i<Books.length; i++) {
+			  if (Books[i] != null && Books[i-1].getPrice() > Books[i].getPrice() )
+				  cheapest = Books[i];
+		 }
+	  return cheapest + "";
+	}
+}
+
